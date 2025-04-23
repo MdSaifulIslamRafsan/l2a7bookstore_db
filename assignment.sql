00fe2472571ce96355c53fd83cb2070b3edf6424
Md Saiful Islam <mdsaifulislamrafsan099@gmail.com>
2025-04-23 08:53:28 +0600

    complete all postgresql query

---
+++ assignment.sql
@@ -0,0 +1,76 @@
+-- Active: 1744395710982@@127.0.0.1@5432@bookstore_db
+CREATE DATABASE bookstore_db;
+
+CREATE TABLE books (
+    id SERIAL PRIMARY KEY,
+    title VARCHAR(50) NOT NULL,
+    author VARCHAR(50) NOT NULL,
+    price INT CHECK(price >= 0),
+    stock INT,
+    published_year INT 
+)
+
+
+CREATE TABLE customers (
+    id  SERIAL PRIMARY KEY,
+    name VARCHAR(50) NOT NULL,
+    email VARCHAR(50) UNIQUE NOT NULL,
+    joined_date DATE DEFAULT CURRENT_DATE
+)
+
+
+
+
+CREATE TABLE orders(
+     id  SERIAL PRIMARY KEY,
+     customer_id INT REFERENCES customers(id) ON DELETE CASCADE,
+     book_id INT REFERENCES books(id) ON DELETE CASCADE,
+     quantity INT CHECK (quantity > 0),
+     order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+)
+
+
+INSERT INTO books (title, author, price, stock, published_year) VALUES
+('The Pragmatic Programmer', 'Andrew Hunt', 40.00, 10, 1999),
+('Clean Code', 'Robert C. Martin', 35.00, 5, 2008),
+('You Don''t Know JS', 'Kyle Simpson', 30.00, 8, 2014),
+('Refactoring', 'Martin Fowler', 50.00, 3, 1999),
+('Database Design Principles', 'Jane Smith', 20.00, 0, 2018);
+
+
+INSERT INTO customers (name, email, joined_date) VALUES
+('Alice', 'alice@email.com', '2023-01-10'),
+('Bob', 'bob@email.com', '2022-05-15'),
+('Charlie', 'charlie@email.com', '2023-06-20');
+
+INSERT INTO orders (customer_id, book_id, quantity, order_date) VALUES
+(1, 2, 1, '2024-03-10'),
+(2, 1, 1, '2024-02-20'),
+(1, 3, 2, '2024-03-05');
+
+
+
+SELECT title FROM books WHERE stock = 0;
+
+SELECT * FROM books ORDER BY price DESC LIMIT 1;
+
+
+
+
+SELECT c.name , COUNT(c.name) AS total_orders FROM customers AS c JOIN orders AS o ON c.id = o.customer_id GROUP BY c.name;
+
+SELECT SUM(b.price * o.quantity) AS total_revenue  FROM books AS b JOIN orders AS o ON o.book_id = b.id;
+
+SELECT c.name, COUNT(c.name) AS orders_count FROM customers AS c JOIN orders AS o ON o.customer_id = c.id GROUP BY c.name HAVING COUNT(c.name) > 1;
+
+
+SELECT AVG(price) AS avg_book_price FROM books;
+
+UPDATE books SET price = price * 1.10 WHERE published_year < 2000;
+
+DELETE FROM customers WHERE id NOT IN (
+    SELECT DISTINCT customer_id FROM orders
+);
+
+
+
